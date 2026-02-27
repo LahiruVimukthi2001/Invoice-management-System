295ca87ffd43e365bde43ad04024d6f0aad23fb9
Lahiru Vimukthi <lahiruvimukthi82@gmail.com>
2026-02-27 21:05:54 +0530

    Create Footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Simple Footer -->
+<footer style="text-align:center; padding:15px; background:#f5f5f5; padding-left:200px; border-top:2px solid #0edf73;">
+    <strong>© <?php echo date('Y'); ?> Invoice Management System</strong> — All Rights Reserved.
+</footer>
+
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
